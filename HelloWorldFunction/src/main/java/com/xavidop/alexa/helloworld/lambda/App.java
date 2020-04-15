798d7ab7e25afaad8dcd0e6a131b2cbef477e220
Xavi <xavi_tb@hotmail.com>
2020-04-15 19:28:43 +0200

    first commit

---
+++ HelloWorldFunction/src/main/java/com/xavidop/alexa/helloworld/lambda/App.java
@@ -0,0 +1,40 @@
+package com.xavidop.alexa.helloworld.lambda;
+
+import com.amazon.ask.Skill;
+import com.amazon.ask.SkillStreamHandler;
+import com.amazon.ask.Skills;
+import com.xavidop.alexa.helloworld.handlers.*;
+import com.xavidop.alexa.helloworld.interceptors.request.LocalizationRequestInterceptor;
+import com.xavidop.alexa.helloworld.interceptors.request.LogRequestInterceptor;
+import com.xavidop.alexa.helloworld.interceptors.response.LogResponseInterceptor;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App extends SkillStreamHandler {
+
+    private static Skill getSkill() {
+        return Skills.standard()
+                .addRequestHandlers(
+                        new CancelandStopIntentHandler(),
+                        new HelloWorldIntentHandler(),
+                        new HelpIntentHandler(),
+                        new LaunchRequestHandler(),
+                        new SessionEndedRequestHandler(),
+                        new FallbackIntentHandler(),
+                        new ErrorHandler())
+                .addExceptionHandler(new MyExceptionHandler())
+                .addRequestInterceptors(
+                        new LogRequestInterceptor(),
+                        new LocalizationRequestInterceptor())
+                .addResponseInterceptors(new LogResponseInterceptor())
+                // Add your skill id below
+                //.withSkillId("[unique-value-here]")
+                .build();
+    }
+
+    public App() {
+        super(getSkill());
+    }
+
+}
